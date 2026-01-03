94928aaf5be2601ffe4c01291ca2db7364cc67d2
GizmoKnPi <kiranpunarthum@gmail.com>
2026-01-03 14:31:28 +0530

    Added camera files

---
+++ launch/camera.launch.py
@@ -0,0 +1,30 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            output='screen',
+            parameters=[{
+                # Explicitly select Logitech C270
+                'video_device': '/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_44725A50-video-index0',
+
+                # Image properties
+                'image_size': [640, 480],
+
+                # Frame ID for TF
+                'camera_frame_id': 'camera_link_optical',
+
+                # Optional but recommended
+                # 'frame_rate': 30.0
+            }]
+        )
+
+    ])
+
